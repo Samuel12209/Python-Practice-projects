55a86be816be2582bfa5c316e852e5edbad714f6
Samuel12209 <samuelbenitez12290@gmail.com>
2024-06-19 09:13:08 -0400

    In progress

---
+++ snake.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+pygame.init()
+
+screen = pygame.display.set_mode(500,500)
+
+
+while True:
+    for event in pygame.event.get():
+        
+        
+        
+        if event.type == pygame.QUIT:
+            exit()
