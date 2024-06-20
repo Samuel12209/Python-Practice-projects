bfbd620469ba2e675abc602fc10d9f04e08b7dd7
Samuel12209 <samuelbenitez12290@gmail.com>
2024-06-20 13:52:53 -0400

    Snake game

--- snake game/snake.py
+++ snake game/snake.py
@@ -4,100 +4,111 @@ import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((700,700))
+screen = pygame.display.set_mode((700, 700))
 screen.fill((0, 150, 0))
 
-snake_color = ((0,255,0))
+snake_color = (0, 255, 0)
 score = 0
-length = 1
 facing = "Right"
-applex = random.randint(10,650)
-appley = random.randint(10,650)
 
+applex = random.randint(10, 650)
+appley = random.randint(10, 650)
 
 fps = pygame.time.Clock()
 
-snake_head = pygame.image.load('snake game\snake_head.png')
-snake_left = pygame.image.load('snake game\snake_headl.png')
-snake_down = pygame.image.load('snake game\snake_headd.png')
-snake_right = pygame.image.load('snake game\snake_headr.png')
-snake_body = pygame.image.load('snake game\snake_body.png')
+snake_head = pygame.image.load('snake game/snake_head.png')
+snake_left = pygame.image.load('snake game/snake_headl.png')
+snake_down = pygame.image.load('snake game/snake_headd.png')
+snake_right = pygame.image.load('snake game/snake_headr.png')
+snake_body = pygame.image.load('snake game/snake_body.png')
 apple = pygame.image.load('snake game/apple.png')
 
+snake_head = pygame.transform.scale(snake_head, (50, 50))
+snake_left = pygame.transform.scale(snake_left, (50, 50))
+snake_right = pygame.transform.scale(snake_right, (50, 50))
+snake_down = pygame.transform.scale(snake_down, (50, 50))
+snake_body = pygame.transform.scale(snake_body, (50, 50))
+apple = pygame.transform.scale(apple, (20, 20))
 
-
-snake_head = pygame.transform.scale(snake_head,(50,50))
-snake_left = pygame.transform.scale(snake_left,(50,50))
-snake_right = pygame.transform.scale(snake_right,(50,50))
-snake_down = pygame.transform.scale(snake_down,(50,50))
-snake_body = pygame.transform.scale(snake_body,(50,50))
-apple = pygame.transform.scale(apple,(20,20))
-
-snakex, snakey = 250,250
-
-
+snakex, snakey = 250, 250
+snake_pos = [snakex, snakey]
+snake_segments = [list(snake_pos)]
+snake_length = 1
 
 def loose(score):
+
     my_font = pygame.font.SysFont('Comic Sans MS', 40)
-    text_surface = my_font.render(('You lost! your score was ' + str(score)), False, (0, 0, 0))
-    screen.blit(text_surface, (120,0))
+    text_surface = my_font.render('You lost! your score was ' + str(score), False, (0, 0, 0))
+    screen.blit(text_surface, (120, 0))
     pygame.display.flip()
     time.sleep(2)
     exit()
 
-def apple_spawn(applex,appley):
-    applex = random.randint(10,650)
-    appley = random.randint(10,650)
-    
+def apple_spawn():
+    global applex, appley
+    applex = random.randint(10, 650)
+    appley = random.randint(10, 650)
+    pygame.display.update()
 
 while True:
-    fps = 60
-    screen.fill((0,150,0))
-    screen.blit(apple,(applex,appley))
-    apple_hitbox = pygame.Rect(applex,appley,applex,appley)
+    screen.fill((0, 150, 0))
+    screen.blit(apple, (applex, appley))
+    apple_hitbox = pygame.Rect(applex, appley, 20, 20)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
-    snake_hitbox = pygame.Rect(snakex,snakey,snakex,snakey)
-    
-    if snake_hitbox.colliderect(apple_hitbox):
-        loose(score)
-    
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d] and snakex < 500:           
-        facing = "Right"
-        print(facing) 
-    if keys[pygame.K_s] and snakey < 380:
-        facing = "Down"
-        print(facing)
-    if keys[pygame.K_w] and snakey > 25:
-        facing = "Up"
-        print(facing)
-    if keys[pygame.K_a] and snakex > 0:
-        facing = "Left"
-        print(facing)
-    
-    if facing == "Right" and snakex < 650:
-        snakex +=0.1
-        screen.blit(snake_right,(snakex,snakey))
-    elif facing == "Left" and snakex > 0:
-        snakex -=0.1
-        screen.blit(snake_left,(snakex,snakey))
-    elif facing == "Down" and snakey < 650:
-        snakey +=0.1
-        screen.blit(snake_down,(snakex,snakey))
-    elif facing == "Up" and snakey > 0:
-        snakey -=0.1
-        screen.blit(snake_head,(snakex,snakey))
 
-    if snakex > 650:
-        loose(score)
-    elif snakex < 0:
-        loose(score)
-    elif snakey > 650:
-        loose(score)
-    elif snakey < 0:
-        loose(score)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:           
+        facing = "Right"
+    if keys[pygame.K_s]:
+        facing = "Down"
+    if keys[pygame.K_w]:
+        facing = "Up"
+    if keys[pygame.K_a]:
+        facing = "Left"
+
+
+    if facing == "Right":
+        snakex += 5
+        screen.blit(snake_right, (snakex, snakey))
+    elif facing == "Left":
+        snakex -= 5
+        screen.blit(snake_left, (snakex, snakey))
+    elif facing == "Down":
+        snakey += 5
+        screen.blit(snake_down, (snakex, snakey))
+    elif facing == "Up":
+        snakey -= 5
+        screen.blit(snake_head, (snakex, snakey))
+
+    snake_pos = [snakex, snakey]
+    snake_segments.append(list(snake_pos))
+    if len(snake_segments) > snake_length:
+        snake_segments.pop(0)
+
+    for segment in snake_segments:
+        screen.blit(snake_body, (segment[0], segment[1]))
+
+    snake_hitbox = pygame.Rect(snakex, snakey, 50, 50)
+    if snake_hitbox.colliderect(apple_hitbox):
+        score += 1
+        snake_length += 10
+        apple_spawn()
     
+    if snakex > 650 or snakex < 0 or snakey > 650 or snakey < 0:
+        loose(score)
+
+    if facing == "Right":
+        screen.blit(snake_right, (snakex, snakey))
+    elif facing == "Left":
+        screen.blit(snake_left, (snakex, snakey))
+    elif facing == "Down":
+        screen.blit(snake_down, (snakex, snakey))
+    elif facing == "Up":
+        screen.blit(snake_head, (snakex, snakey))
+
     pygame.display.flip()
+    fps.tick(60)
